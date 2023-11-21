8ec01c042c682756956af2bca27533b473240c50
jacopobiagi <jacopo.biagi02@gmail.com>
2023-11-21 14:36:18 +0100

    aggiunta di commenti

--- calc-ticket.js
+++ calc-ticket.js
@@ -1,16 +1,23 @@
+//variabili input
 let nome = document.getElementById("nomePasseggero");
 let distanza = document.getElementById("distanza");
 let eta = document.getElementById("etaPasseggero");
+
+//variabile del secondo blocco
 let biglietto = document.getElementById("biglietto");
+
+//variabile dei blocchi da inserire
 let costo = document.getElementById("costoComplessivo");
 let passeggero = document.getElementById("mioNome");
 
 
+//al click del tasto genera
 document.getElementById("submit").addEventListener("click",
+
     function(){
 
         //Controllo se i campi sono stati tutti riempiti
-        if(nome.value && distanza.value && eta.value!=="0"){
+        if(nome.value && distanza.value && eta.value !== "0"){
 
             let distanzaNumerica = parseInt(distanza.value);//conversione del valore
 
@@ -24,19 +31,24 @@ document.getElementById("submit").addEventListener("click",
                 let prezzoLordo = distanzaNumerica * 0.21;
                 let prezzoNetto = 0;
 
-
+                //caso minorenne
                 if(eta.value == '1'){
+
                     prezzoNetto = prezzoLordo - (prezzoLordo * 0.2);
 
-                }else if(eta.value == '3'){
-                    prezzoNetto = prezzoLordo - (prezzoLordo * 0.45);
+                //caso over 65
+                } else if(eta.value == '3'){
 
+                    prezzoNetto = prezzoLordo - (prezzoLordo * 0.45);
                 }
+                //caso maggiorenne
                 else{
+
                     prezzoNetto = prezzoLordo;
                 }
                 
                 distanza.style.borderColor = "#ced4da";
+
                 costo.innerHTML =  prezzoNetto.toFixed(2);
                 passeggero.innerHTML = nome.value;
 
@@ -52,6 +64,7 @@ document.getElementById("submit").addEventListener("click",
         
     }
 );
+
 
 document.getElementById("delete").addEventListener("click",
 
